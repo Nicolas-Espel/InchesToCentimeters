a40f904005317a25469b042afd35c31302f9463b
Nicolas-Espel <101594373+Nicolas-Espel@users.noreply.github.com>
2023-11-29 01:20:44 +0000

    Re-factored program to be hard coded.

--- main.cs
+++ main.cs
@@ -4,8 +4,7 @@ class Program {
   public static void Main (string[] args) {
     const double CentimetersInAnInch = 2.54;
     
-    Console.WriteLine ("Enter how many inches to convert: ");
-    var inches = Convert.ToInt16(Console.ReadLine());
+    var inches = 15994;
 
     var conversion = inches * CentimetersInAnInch;
     Console.WriteLine("{0} inches is {1} centimeters.", inches, conversion);
